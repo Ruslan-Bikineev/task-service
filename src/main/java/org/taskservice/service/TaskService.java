221f0d966babb532c2ef2556dff7880653d2b1d6
Ruslan Bikineev <ruslan.bikineev@simbirsoft.com>
2025-02-17 23:09:01 +0300

    Ruslan Bikineev: added task service

--- src/main/java/org/taskservice/service/TaskService.java
+++ src/main/java/org/taskservice/service/TaskService.java
@@ -1,7 +1,11 @@
 package org.taskservice.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.taskservice.entity.Task;
 import org.taskservice.repository.TaskRepository;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -9,5 +13,32 @@ public class TaskService {
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+
+    public Task getById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Task with id " + id + " not found"));
+    }
+
+    public List<Task> getAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task put(Task task) {
+        if (taskRepository.existsById(task.getId())) {
+            return taskRepository.save(task);
+        } else {
+            throw new EntityNotFoundException(
+                    "Task with id " + task.getId() + " not found");
+        }
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
     }
 }
