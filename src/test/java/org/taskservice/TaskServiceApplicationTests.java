dbca97f8fd93015954ac6c29b8d5f810c426da8f
Ruslan Bikineev <ruslan.bikineev@simbirsoft.com>
2025-03-23 22:55:37 +0300

    Ruslan Bikineev: update up kafka with testcontainer

--- src/test/java/org/taskservice/TaskServiceApplicationTests.java
+++ src/test/java/org/taskservice/TaskServiceApplicationTests.java
@@ -1,15 +1,33 @@
 package org.taskservice;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @DirtiesContext
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class TaskServiceApplicationTests {
+    private static ConfluentKafkaContainer KAFKA_CONTAINER;
+
+    @BeforeAll
+    static void setUpBeforeAll() {
+        KAFKA_CONTAINER = new ConfluentKafkaContainer(
+                DockerImageName.parse("confluentinc/cp-kafka:7.5.1")
+                        .asCompatibleSubstituteFor("apache/kafka")
+        );
+        KAFKA_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void configureKafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("kafka.task.bootstrap-server", KAFKA_CONTAINER::getBootstrapServers);
+    }
 }
--- src/test/java/org/taskservice/controller/TaskControllerTest.java
+++ src/test/java/org/taskservice/controller/TaskControllerTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class TaskControllerTest extends TaskServiceApplicationTests {
+public class TaskControllerTest extends TaskServiceApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
